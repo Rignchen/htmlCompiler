63b540f35ee33e1880d70b233bad7a0bc01732ab
Rignchen <rinchendemort@gmail.com>
2023-12-24 15:33:08 +0100

    implement auto compiling

--- script/compiler.py
+++ script/compiler.py
@@ -6,6 +6,13 @@ class compiler:
 	def __init__(self, settings):
 		self.settings = settings.get
 	def start(self):
+		while self.settings["autoCompile"]:
+			end = datetime.now() + timedelta(seconds=self.settings["compileDelay"])
+			self.test_compile()
+			while datetime.now() < end: pass
+		else:
+			self.test_compile()
+	def test_compile(self):
 		print("\033[94mLooking for files to compile...\033[0m")
 		# compile all the files from the subfolder "run"
 		for (path, dirs, files) in walk("run"):
