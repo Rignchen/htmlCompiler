8ff2bf44e3a513346598b9a842f186003ccc2a6c
Rignchen <rinchendemort@gmail.com>
2023-12-24 16:35:17 +0100

    create copy inside compiled

--- script/compiler.py
+++ script/compiler.py
@@ -1,16 +1,18 @@
 from json import load, dump
 from datetime import datetime, timedelta
-from os import walk, path as osPath
+from os import makedirs, walk, path as osPath, remove
+from shutil import copy
 
 class compiler:
 	def __init__(self, settings):
 		self.settings = settings.get
 		with open("htmlCompilerCache.json", "r") as f:
-			self.cache = load(f)
+			self.cache: dict[str,str] = load(f)
 	def save(self):
 		with open("htmlCompilerCache.json", "w") as f:
 			dump(self.cache, f, indent="\t")
 	def start(self):
+		print("\033[94mStart compiler...\033[0m")
 		while self.settings["autoCompile"]:
 			end = datetime.now() + timedelta(seconds=self.settings["compileDelay"])
 			self.test_compile()
@@ -26,6 +28,7 @@ class compiler:
 			if not osPath.exists(cacheFile):
 				self.change = True
 				print(f"\nDeleting {cacheFile}...",end="")
+				remove(f"compiled/{cacheFile.removeprefix('run/')}")
 				del self.cache[cacheFile]
 		for (path, dirs, files) in walk("run"):
 			for file in files:
@@ -34,15 +37,20 @@ class compiler:
 					fileContent = f.read()
 				if filePath in self.cache:
 					if self.cache[filePath] != fileContent:
-						self.compile(filePath,fileContent)
+						self.compileFile(filePath,fileContent)
 				else:
-					self.compile(filePath,fileContent)
+					self.compileFile(filePath,fileContent)
 		if self.change:
 			print()
 			self.save()
 		else:
 			print("\r\033[91mNo changes detected    \033[0m",end="")
-	def compile(self, filePath: str, fileContent: str):
+	def compileFile(self, filePath: str, fileContent: str):
 		self.change = True
 		print(f"\nCompiling {filePath}...",end="")
 		self.cache[filePath] = fileContent
+
+		compiledFilePath = f"compiled/{filePath.removeprefix('run/')}"
+		makedirs(osPath.dirname(compiledFilePath), exist_ok=True)
+
+		copy(filePath, compiledFilePath)
