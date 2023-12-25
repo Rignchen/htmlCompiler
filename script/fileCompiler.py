b4ab9219120bef1ea197788f959c70497d92b185
Rignchen <rinchendemort@gmail.com>
2023-12-25 18:10:26 +0100

    ability to get the list of model to apply

--- script/fileCompiler.py
+++ script/fileCompiler.py
@@ -1,10 +1,19 @@
 def compileHTML(fileContent, filePath, models):
-	model = getModel(models, filePath)
-	return fileContent
+	modelList = getModelList(models, filePath)
+	
 def compileModels(fileContent: str) -> list[str]:
 	return [fileContent]
-def getModel(modelList: dict[str,list[str]], filePath: str) -> str:
-	return modelList[filePath][0]
+def getModelList(modelList: dict[str,list[str]], filePath: str) -> list[list[str]]:
+	path = filePath.replace("\\","/").split("/")
+	pathList: list[list[str]] = []
+	for i in range(len(path)-1,-1,-1):
+		currentPath = "/".join(path[:i+1])
+		if currentPath in modelList:
+			pathList.append(modelList[currentPath])
+			# find the last model where the list has a length of 1
+			if len(pathList[-1]) == 1: 
+				pathList.reverse()
+				return pathList
 def pasteModel(content: str, model: list[str]) -> str:
 	content: list[str] = content.split("<model/>")
 	for i in range(len(model)):
