7fbb047afc96bc76f9082afbf6a5b885508b82d2
Rignchen <rinchendemort@gmail.com>
2023-12-25 20:25:29 +0100

    ability to compile the html file with the models

--- script/fileCompiler/htmlFile.py
+++ script/fileCompiler/htmlFile.py
@@ -1,5 +1,8 @@
-def compileHTML(fileContent, filePath, models):
-	modelList = getModelList(models, filePath)
+def compileHTML(fileContent: str, filePath: str, models: dict[str,str]) -> str:
+	fileContent = removeComments(fileContent)
+	if "<model>" in fileContent:
+		return pasteModel(getModel(models,filePath),splitModels(fileContent))
+	return fileContent
 def removeComments(fileContent: str) -> str:
 	while "<!--" in fileContent:
 		fileContent = fileContent[:fileContent.index("<!--")] + fileContent[fileContent.index("-->")+3:]
@@ -8,20 +11,15 @@ def removeComments(fileContent: str) -> str:
 def splitModels(fileContent: str) -> list[str]:
 	models = [model.split("</model>")[0] for model in fileContent.split("<model>")]
 	return models[1:] if len(models) > 1 else models
-def getModelList(modelList: dict[str,list[str]], filePath: str) -> list[list[str]]:
+def getModel(modelList: dict[str,list[str]], filePath: str) -> str:
 	path = filePath.replace("\\","/").split("/")
-	pathList: list[list[str]] = []
 	for i in range(len(path)-1,-1,-1):
 		currentPath = "/".join(path[:i+1])
 		if currentPath in modelList:
-			pathList.append(modelList[currentPath])
-			# find the last model where the list has a length of 1
-			if len(pathList[-1]) == 1: 
-				pathList.reverse()
-				return pathList
-def pasteModel(content: str, model: list[str]) -> str:
-	content: list[str] = content.split("<model/>")
-	for i in range(len(model)):
-		if len(content) == 1: break
-		content = [content.pop(0) + model[i] + content.pop(0)] + content
-	return "".join(content)
+			return modelList[currentPath]
+def pasteModel(model: str, contents: list[str]) -> str:
+	model: list[str] = model.split("<model/>")
+	for i in range(len(contents)):
+		if len(model) == 1: break
+		model = [model.pop(0) + contents[i] + model.pop(0)] + model
+	return "".join(model)
