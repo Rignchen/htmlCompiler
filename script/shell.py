2268f743487504ab6e1983f3970e5802bb52ad12
Rignchen <rinchendemort@gmail.com>
2023-12-26 00:24:05 +0100

    Update help message

--- script/shell.py
+++ script/shell.py
@@ -17,6 +17,7 @@ class shell:
 			"clear - clears the screen",
 			"compile - starts the compiler",
 			"settings - opens the settings menu",
+			"clearCache - Empty the whole cache"
 			"exit - exits the program",
 		], " - "))
 
