b3951e2f5d0eeee22e3811165cd2486a253b691b
Rignchen <rinchendemort@gmail.com>
2023-12-30 21:46:53 +0100

    format css

--- script/fileCompiler/cssFile.py
+++ script/fileCompiler/cssFile.py
@@ -4,4 +4,17 @@ def compileCss(fileContent: str) -> str:
 	fileContent = removeComments(fileContent,"/*","*/")
 	return formatCss(fileContent)
 def formatCss(fileContent: str):
-	return fileContent
+	content = [i.split("{") for i in fileContent.split("}")[:-1]]
+	
+	i = 0
+	while i < len(content):
+		content[i] = [
+			",\n".join([j.strip() for j in content[i][0].split(",")]),
+			"\n\t" + ";\n\t".join(["".join([k.strip() for k in j.split("\n")]) for j in content[i][1].split(";")])[:-1]
+		]
+		if content[i][1] == "\n\t":
+			content.pop(i)
+			continue
+		i += 1
+	
+	return "\n".join([i[0] + " {" + i[1] + "}" for i in content])
