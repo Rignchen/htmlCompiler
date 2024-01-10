6b3fb1d1e24cb0248a6b66bb5bd2b3d3763396ef
Rignchen <rinchendemort@gmail.com>
2024-01-10 21:38:22 +0100

    allow to do stuff while localhost is closing

--- script/lib/localhost.py
+++ script/lib/localhost.py
@@ -4,12 +4,26 @@ from socketserver import TCPServer
 import sys
 from threading import Thread
 
+class Pipe:
+	def __init__(self):
+		self._value = []
+	def __len__(self):
+		return len(self._value)
+	def __iter__(self):
+		yield self._value.pop(0)
+	def send(self,value):
+		self._value.append(value)
+	def peak(self) -> list:
+		return self._value
+	def hasValues(self):
+		return self.__len__() > 1
+
 class _CustomHttpHandler(SimpleHTTPRequestHandler):
 	dir: str = "."
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs, directory=self.dir)
 class localhostThread(Thread):
-	def __init__(self, directory: str = ".", port: int = 8000, force_port: bool = False, pipe: list|None = None):
+	def __init__(self, directory: str = ".", port: int = 8000, force_port: bool = False, pipe: Pipe|None = None):
 		"""
 		Start a new localhost
 		"""
@@ -40,28 +54,30 @@ class localhostThread(Thread):
 			self.httpd.serve_forever()
 		finally:
 			sendMessage("Localhost stopped", self.pipe)
-def sendMessage(message, pipe: list|None = None):
+def sendMessage(message, pipe: Pipe|None = None):
 	if pipe == None: print(message)
-	else: pipe.append(message)
-def printMessage(pipe: list = None, min_message: int = 0):
+	else: pipe.send(message)
+def printMessage(pipe: Pipe, min_message: int = 1):
 	while len(pipe) < min_message: pass
-	while len(pipe) > 0: print(pipe.pop(0))
+	for message in pipe:
+		print(message)
 def startLocalhost(directory: str = ".", port: int = 8000, force_port: bool = False) -> tuple[Thread, list]:
 	"""
 	Start a new localhost on a new Thread\n
 	It return the process and pipe used for the localhost, both of them are needed to stop this one
 	"""
-	pipe = []
+	pipe = Pipe()
 	process = localhostThread(directory, port, force_port, pipe)
 	process.start()
 	printMessage(pipe, 1)
 	return process, pipe
-def stopLocalhost(localhost: tuple[localhostThread, list]) -> None:
+def stopLocalhost(localhost: tuple[localhostThread, Pipe]) -> None:
 	"""
 	Stop a thread with the localhost\n
 	The input is the return of startLcalhost()
 	"""
-	process, pipe = localhost
 	print("Shutting down host, this may take a few minutes")
+	_stopLocalHost(*localhost)
+async def _stopLocalHost(process: localhostThread, pipe: Pipe) -> None:
 	process.stop()
 	printMessage(pipe)
